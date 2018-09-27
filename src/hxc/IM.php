3dfcfe6bc8dbfe6d743142393375b99bc6b01834
hxc <945120753@qq.com>
2018-09-27 21:08:22 +0800

    融合进TP3.2.3

--- src/hxc/IM.php
+++ src/hxc/IM.php
@@ -10,35 +10,26 @@ namespace HXC;
 
 use Tencent\TLSSigAPI;
 
-define('BASE_PATH',str_replace( '\\' , '/' , realpath(dirname(__FILE__).'/../../')).'/');
+define('BASE_PATH',str_replace( '\\' , '/' , realpath(dirname(__FILE__).'/../../../../../')).'/');
 
 class IM
 {
-    static private $config = [];
+    static private $identifier;
 
-
-    public function aa()
-    {
-        echo 11;
-    }
 
     /**
-     * 初始化
-     * @param $config 配置数据
-     * $key = [
-     *      'appid' => '',
-     *      'path' =>
-     *  ];
-     * @return $this
+     * 用户
+     * @param $identifier
+     * @return IM
      */
-    public static function init($config)
+    public static function identifier($identifier)
     {
         try{
-            if(is_array($config)){
-                self::$config = $config;
+            if($identifier){
+                self::$identifier = $identifier;
                 return new IM();
             }else{
-                throw new \Error('$config必须为数组');
+                throw new \Error('$identifier不能为空');
             }
         }catch (\Error $e){
             echo $e;
@@ -46,25 +37,41 @@ class IM
     }
 
 
+
     public function genSig()
     {
-        $config = self::$config;
-        $private_key = file_get_contents(BASE_PATH.$config['path'].'/private_key');
-        $public_key = file_get_contents(BASE_PATH.$config['path'].'/public_key');
+        $cache = S([
+            'prefix'=>'usersig',
+            'expire'=>15550000
+        ]);
+        $identifier = self::$identifier;
+        $usersig = $cache->$identifier;
+        if(!$usersig){
+            $config = C('im');
+            $private_key = file_get_contents(BASE_PATH.$config['path'].'/private_key');
+            $public_key = file_get_contents(BASE_PATH.$config['path'].'/public_key');
+            $api = new TLSSigAPI();
+            $api->SetAppid($config['appid']);//设置在腾讯云申请的appid
+            $api->SetPrivateKey($private_key);//生成usersig需要先设置私钥
+            $api->SetPublicKey($public_key);//校验usersig需要先设置公钥
+            $sig = $api->genSig($identifier);//生成usersig
+            $cache->$identifier = $sig;
+            return $sig;
+        }
+        return $usersig;
+    }
+
+    /**
+     * 校验usersig
+     */
+    public function verifySig($sig)
+    {
         $api = new TLSSigAPI();
-        $api->SetAppid($config['appid']);//设置在腾讯云申请的appid
-        $api->SetPrivateKey($private_key);//生成usersig需要先设置私钥
-        $api->SetPublicKey($public_key);//校验usersig需要先设置公钥
-        $sig = $api->genSig($config['identifier']);//生成usersig
-        echo $sig;
-        $sig = 'eJxNjF1vgjAYRv9Lb13M25YOtjtElhFxH0F086ZhUrBTsGkLKmb-fYRotttznvNc0CJOxtlmc2hqy*1ZCfSIAN0NWOaitrKQQvfQCmPxVWRKyZxnllOd-9ubfMcH1TPsAGCHuR67SnFSUgueFXa4w4wxAnBLW6GNPNS9IIAZJhTgT1pZiSGhHtB7wm6XRpY9noefQfQeuCLpAjMdrWfRKF1tg9ly8uw8eFrRNtyXTXq0RXqmX1289KOtH2MPdn5CKvdYf8-bpxejGmOqxespBDfpVh*xWe8nEXsrffTzC*s2V9M_';
-        echo '<br /><br /><br />';
-        $result = $api->verifySig($sig, 'test1', $init_time, $expire_time, $error_msg);//校验usersig
+        $result = $api->verifySig($sig, self::$identifier, $init_time, $expire_time, $error_msg);//校验usersig
         var_dump($result);
         var_dump($init_time);
         var_dump($expire_time);
         var_dump($error_msg);
-
     }
 
 
