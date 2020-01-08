f19310061e3303f741fad41f77ddfa915fd896a9
hxc <3365997294@qq.com>
2020-01-08 13:14:28 +0800

    升级为v2版本

--- src/Provider/TX.php
+++ src/Provider/TX.php
@@ -15,21 +15,24 @@ class TX implements Base
     protected $request = [];//请求的参数
     //腾讯云方法
     protected $funArr = [
-        'portrait_set'           => '/v4/profile/portrait_set',//设置资料
-        'portrait_get'           => '/v4/profile/portrait_get',//拉取资料
-        'account_import'         => '/v4/im_open_login_svc/account_import',//单个帐号导入
-        'multiaccount_import'    => '/v4/im_open_login_svc/multiaccount_import',//批量帐号导入
-        'create_group'           => '/v4/group_open_http_svc/create_group',//创建群组
-        'get_appid_group_list'   => '/v4/group_open_http_svc/get_appid_group_list',//获取App中的所有群组
-        'destroy_group'          => '/v4/group_open_http_svc/destroy_group',//解散群
-        'delete_group_member'    => '/v4/group_open_http_svc/delete_group_member',//删除群组成员
-        'add_group_member'       => '/v4/group_open_http_svc/add_group_member',//增加群组成员
-        'get_joined_group_list'  => '/v4/group_open_http_svc/get_joined_group_list',//获取用户所加入的群组
-        'modify_group_base_info' => '/v4/group_open_http_svc/modify_group_base_info',//修改群组基础资料
-        'get_group_member_info'  => '/v4/group_open_http_svc/get_group_member_info',//获取群组成员详细资料
-        'get_group_info'         => '/v4/group_open_http_svc/get_group_info',//获取群组详细资料
-        'friend_import'          => '/v4/sns/friend_import',//导入好友
-        'account_delete'         => '/v4/im_open_login_svc/account_delete',//账号删除
+        'portrait_set'             => '/v4/profile/portrait_set',//设置资料
+        'portrait_get'             => '/v4/profile/portrait_get',//拉取资料
+        'account_import'           => '/v4/im_open_login_svc/account_import',//单个帐号导入
+        'multiaccount_import'      => '/v4/im_open_login_svc/multiaccount_import',//批量帐号导入
+        'create_group'             => '/v4/group_open_http_svc/create_group',//创建群组
+        'get_appid_group_list'     => '/v4/group_open_http_svc/get_appid_group_list',//获取App中的所有群组
+        'destroy_group'            => '/v4/group_open_http_svc/destroy_group',//解散群
+        'delete_group_member'      => '/v4/group_open_http_svc/delete_group_member',//删除群组成员
+        'add_group_member'         => '/v4/group_open_http_svc/add_group_member',//增加群组成员
+        'get_joined_group_list'    => '/v4/group_open_http_svc/get_joined_group_list',//获取用户所加入的群组
+        'modify_group_base_info'   => '/v4/group_open_http_svc/modify_group_base_info',//修改群组基础资料
+        'get_group_member_info'    => '/v4/group_open_http_svc/get_group_member_info',//获取群组成员详细资料
+        'get_group_info'           => '/v4/group_open_http_svc/get_group_info',//获取群组详细资料
+        'friend_import'            => '/v4/sns/friend_import',//导入好友
+        'account_delete'           => '/v4/im_open_login_svc/account_delete',//账号删除
+        'modify_group_member_info' =>'v4/group_open_http_svc/modify_group_member_info',//修改群成员资料
+        'change_group_owner'       => 'v4/group_open_http_svc/change_group_owner' //转让群组
+        'friend_check'             => '/v4/sns/friend_check',//校验好友
     ];
 
     /**
@@ -72,30 +75,16 @@ class TX implements Base
      */
     public function genSig()
     {
-        $config = $this->config;
-        $api = self::getApi();
         $sig = Cache::tag('TX-IM')->get($this->userName);
         if(!$sig){//缓存中没有签名
+            $config = $this->config;
+            $api = new TLSSigAPIv2($this->config['appid'],$this->config['secret']);;
             $sig = $api->genSig($this->userName);//生成usersig
             Cache::tag('TX-IM')->set($this->userName,$sig,15551000);
         }
         return $sig;
     }
 
-    /**
-     * @return TLSSigAPI
-     * @throws \Exception
-     */
-    private function getApi()
-    {
-        $api = new TLSSigAPI();
-        $api->SetAppid($this->config['appid']);//设置在腾讯云申请的appid
-        $private_key = file_get_contents(ROOT_PATH.$this->config['path'].'/private_key.txt');
-        $public_key = file_get_contents(ROOT_PATH.$this->config['path'].'/public_key.txt');
-        $api->SetPrivateKey($private_key);//生成usersig需要先设置私钥
-        $api->SetPublicKey($public_key);//校验usersig需要先设置公钥
-        return $api;
-    }
 
     /**
      * @param $fun
